4ad3a81af21ee7b5612ce95d8150a09055e2382f
Serina Ko <serinako08@gmail.com>
2018-09-28 21:39:33 +1000

    convert the contacts object to an array when passed in as props using .values method

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Sidebar from './Sidebar';
 import Main from './Main';
-
 import './App.css';
+import store from './store';
+import  _ from lodash
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Sidebar />
-        <Main />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const { contacts } = store.getState();
+
+  // .values method: it creates an array out of all key values of the object passed in
+  return (
+    <div className="App">
+      <Sidebar contacts={_.values(contacts)} />
+      <Main />
+    </div>
+  );
+};
 
 export default App;
