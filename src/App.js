b1bbdfdeac6764299d22ba54f41ebd49205bab4c
Serina Ko <serinako08@gmail.com>
2018-09-30 16:08:22 +1000

    fix error

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Sidebar from './Sidebar';
 import Main from './Main';
 import './App.css';
 import store from './store';
-import  _ from lodash
+import _ from 'lodash';
 
 const App = () => {
   const { contacts } = store.getState();
