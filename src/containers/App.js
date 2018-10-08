656e76e161f16d69078774d14008b0ae78f75ec2
Serina Ko <serinako08@gmail.com>
2018-10-08 13:40:11 +1100

    retrieve the user and activeUserId from the store

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,13 +6,13 @@ import store from '../store';
 import _ from 'lodash';
 
 const App = () => {
-  const { contacts } = store.getState();
+  const { contacts, user, activeUserId } = store.getState();
 
   // .values method: it creates an array out of all key values of the object passed in
   return (
     <div className="App">
       <Sidebar contacts={_.values(contacts)} />
-      <Main />
+      <Main user={user} activeUserId={activeUserId} />
     </div>
   );
 };
